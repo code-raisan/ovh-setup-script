c730e18569fb0ab9388804734f2f6f3a474b7c9b
Raisan <yuri@rainyan.net>
2023-02-18 16:11:26 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
+echo -n USER_NAME: 
+read username
+echo -n PASSWORD:
+read password
+
 apt update -y
 apt upgrade -y
 apt dist-upgrade
 
+adduser --disabled-password --gecos "" "$username"
+echo "${username}:${username}" | chpasswd
+gpasswd -a "$password" sudo
